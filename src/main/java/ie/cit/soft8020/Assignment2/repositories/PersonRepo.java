8556b271617f4429e93b5d38749c4c6576efa421
r_meany <ryan.meany@mycit.ie>
2017-11-08 09:32:43 +0000

    First commit

---
+++ src/main/java/ie/cit/soft8020/Assignment2/repositories/PersonRepo.java
@@ -0,0 +1,8 @@
+package ie.cit.soft8020.Assignment2.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import ie.cit.soft8020.Assignment2.basepackage.entities.Person;
+
+public interface PersonRepo extends MongoRepository<Person, Integer>{
+
+}
